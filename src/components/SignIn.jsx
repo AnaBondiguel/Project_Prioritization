2e4dd27f3f513dc05ea49e3b9813c6da0a26d5ce
AnaBondiguel <michaelyangyang@hotmail.com>
2022-10-29 00:22:06 +1100

    styled signin and signup pages

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
 import Button from "@mui/material/Button";
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
 
 function SignIn(){
     const initialFormState = {
@@ -22,14 +29,29 @@ function SignIn(){
           }
      
     return (
-        <div>
-            <h1>Sign In</h1>
-            <label>Email:</label><input type="email" name="email" value={formState.username} onChange={handleChange}></input>
-                <br></br> <br></br>
-            <label>Password:</label><input type="password" name="password" value={formState.password} onChange={handleChange}></input>
-                <br></br> <br></br>
-            <Button onClick={handleSubmit}>Sign in</Button>
-        </div>
+        <Box sx={{ ml:70, width: 500, height: 500, backgroundColor: '#85C1E9 '}} >
+            <Container maxWidth="lg">
+                <Typography variant="h6" align="center" gutterBottom>
+                    <h1>Sign In</h1>
+                    <label>Email:</label><input type="email" name="email" value={formState.username} onChange={handleChange}></input>
+                        <br></br> <br></br>
+                    <label>Password:</label><input type="password" name="password" value={formState.password} onChange={handleChange}></input>
+                        <br></br> <br></br>
+                    <FormControlLabel control={<Checkbox defaultChecked />} label="Remember me?" />
+                        <br></br> <br></br>
+                    <Button variant="contained" size="large" onClick={handleSubmit}>Sign in</Button>
+                        <br></br> 
+                    <Link href="#">Forgot Password?</Link>
+                        <br></br> <br></br>
+                    <Grid container spacing={1} columns={8}>
+                        <Grid item xs={4}>Need an account?</Grid>
+                        <Grid item xs={4}>
+                            <Link href="/Signup">Sign up</Link>
+                        </Grid>
+                    </Grid>
+                </Typography>
+            </Container>
+        </Box>
     );
 }
 
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
 import Button from "@mui/material/Button";
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Link from '@mui/material/Link';
 
 function SignUp(){
     const initialFormState = {
@@ -7,6 +12,7 @@ function SignUp(){
         lastname: "",
         email:"",
         password:"",
+        comfirmpassword:"",
       };
     
       const [formState, setFormState] = useState(initialFormState);
@@ -26,19 +32,36 @@ function SignUp(){
                 }
 
     return (
-        <div>
-            <h1>Sign Up</h1>
-            <label>First Name:</label><input type="name" name="firstname" value={formState.firstname} onChange={handleChange}></input>
-                <br></br> <br></br>
-            <label>Last Name:</label><input type="name" name="lastname" value={formState.lastname} onChange={handleChange}></input>
-                <br></br> <br></br>
-            <label>Email:</label><input type="email" name="email" value={formState.username} onChange={handleChange}></input>
-                <br></br> <br></br>
-            <label>Password:</label><input type="password" name="password" value={formState.password} onChange={handleChange}></input>
-                <br></br> <br></br>
-            <Button onClick={handleSubmit}>Sign up</Button>
-        </div>
+        <Box sx={{ml: 70, width: 550, height: 550, backgroundColor: '#85C1E9 '}} >
+            <Container maxWidth="lg">
+                <Typography variant="h6" align="center" gutterBottom>
+                    <h1>Sign Up</h1>
+                    <label>First Name:</label><input type="name" name="firstname" value={formState.firstname} onChange={handleChange}></input>
+                        <br></br>  <br></br> 
+                    <label>Last Name:</label><input type="name" name="lastname" value={formState.lastname} onChange={handleChange}></input>
+                        <br></br>  <br></br> 
+                    <label>Email:</label><input type="email" name="email" value={formState.username} onChange={handleChange}></input>
+                        <br></br>  <br></br> 
+                    <label>Password:</label><input type="password" name="password" value={formState.password} onChange={handleChange}></input>
+                        <br></br>  <br></br> 
+                    <label>Comfirm Password:</label><input type="password" name="confirmpassword" value={formState.comfirmpassword} onChange={handleChange}></input>
+                        <br></br>  <br></br> 
+                    <Button variant="contained" onClick={handleSubmit}>Sign up</Button>
+                        <br></br> <br></br> 
+                    <Grid container spacing={1} columns={8}>
+                        <Grid item xs={4}>Already a user?</Grid>
+                        <Grid item xs={4}>
+                            <Link href="/Signin">Sign in</Link>
+                        </Grid>
+                    </Grid>
+                </Typography>
+            </Container>
+        </Box>
     );
 }
 
 export default SignUp;
+
+
+
+
