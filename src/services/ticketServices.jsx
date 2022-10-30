34bf66889587d24acc78b954b3603d04eefadef7
AnaBondiguel <michaelyangyang@hotmail.com>
2022-10-31 00:54:39 +1100

    Added get a single ticket

--- src/services/ticketServices.jsx
+++ src/services/ticketServices.jsx
@@ -1,5 +1,5 @@
 const tickets = [
-    {id: 1, initiative: "Whiteboard on Pricing LP", description: "Add whiteboard icon to the pricing page", target: "Team", type:"I", impact: "Medium", confidence: "Large", effort: "small", ICE_Score:"24",  priority:"1"},
+    {id: 1, initiative: "Whiteboard on Pricing LP", description: "Add whiteboard icon to the pricing page", target: "Team", type:"I", impact: "Medium", confidence: "Large", effort: "small", dueDate: "2022-01-11T01:33:50.019Z", user_id: 1, ICE_Score:"24",  priority:"1", feedback:"Great work"},
 ]
 
 function transformTicket(ticket) {
@@ -19,21 +19,31 @@ function transformTicket(ticket) {
     }
 }
 
+//get all tickets
 export async function getTickets() {
   
     return tickets;
 }
 
+//get a single ticket
+export async function getPrediction(id) {
+    const ticket = tickets.find(ticket => ticket.id.toString() === id.toString())
+    return ticket ? transformTicket(ticket) : null
+}
+
+//create a ticket
 export async function createTicket(ticket) {
 	
     return ticket;
 }
 
+//delete a ticket
 export async function deleteTicket(id) {
 
     return id;
 }
 
+//update a ticket
 export async function updateTicket(ticket) {
      return ticket;
 }
