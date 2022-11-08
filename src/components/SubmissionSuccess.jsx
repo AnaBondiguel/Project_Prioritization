32dba218abac448cad842cda0d9346ba474e8cd1
AnaBondiguel <michaelyangyang@hotmail.com>
2022-11-08 22:40:02 +1100

    changed submission success page

--- src/components/SubmissionSuccess.jsx
+++ src/components/SubmissionSuccess.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 function SubmissionSuccess(){
     return (
         <div>
             <h1>Thank You!</h1>
             <h3>Your ticket has been submitted successfully</h3>
+            <Link to="/mytickets">Go back to My Ticket</Link>
         </div>
     );
 }
